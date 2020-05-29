44cd4de2194921544c675b62c6e2e5f5f615f7d0
Ankesh Verma <ankesh1989@gmail.com>
2020-05-29 16:20:05 +0530

    COmmit

--- src/app/sticky.component.ts
+++ src/app/sticky.component.ts
@@ -1,27 +1,27 @@
-import { Component, Input } from '@angular/core';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import { FormGroup, FormBuilder } from '@angular/forms';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { Component, Input } from "@angular/core";
+//import {MatSidenavModule} from '@angular/material/sidenav';
+import { FormGroup, FormBuilder } from "@angular/forms";
+//import {MatFormFieldModule} from '@angular/material/form-field';
 @Component({
-  selector: 'sticky',
-  templateUrl: './sticky.component.html',
- styleUrls: [ './sticky.component.css' ]
+  selector: "sticky",
+  templateUrl: "./sticky.component.html",
+  styleUrls: ["./sticky.component.css"],
 })
-export class StickyComponent  {
- showFiller = false;
+export class StickyComponent {
+  showFiller = false;
   noteData: any = {};
- notesForm: FormGroup;
-  
-  constructor(private formBuilder: FormBuilder) { }
+  notesForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    console.log("sticky");
     this.notesForm = this.formBuilder.group({
-      note: ''
-     
-    })
+      note: "",
+    });
   }
-valuechange(newValue) {
-  this.noteData['note'] = newValue;
-  console.log(newValue)
-}
+  valuechange(newValue) {
+    this.noteData["note"] = newValue;
+    console.log(newValue);
+  }
 }
