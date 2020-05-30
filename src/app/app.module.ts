a0c0d03faee9b4dc6c0b0a38b2b6d1270f22f6b6
Ankesh Verma <ankesh1989@gmail.com>
2020-05-30 16:28:42 +0530

    x

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,107 @@
-import { NgModule } from '@angular/core';
-import { FormsModule ,ReactiveFormsModule} from '@angular/forms';
-import { AppComponent } from './app.component';
-import { StickyComponent } from './sticky.component';
-import {BrowserModule} from '@angular/platform-browser';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppComponent } from "./app.component";
+
+import { StickyComponent } from "./sticky.component";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { StoreModule } from "@ngrx/store";
+import { reducers } from "./store";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import {
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+} from "@angular/material";
+import { ConfirmationDialog } from "./confirmation-dialog.component";
 
 @NgModule({
-  
-  imports:      [ BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule,MatSidenavModule,MatIconModule,MatToolbarModule,
-    MatFormFieldModule,ReactiveFormsModule],
-  declarations: [ AppComponent, StickyComponent ],
-  bootstrap:    [ AppComponent ]
+  exports: [
+    // Material
+
+    // Material
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSlideToggleModule,
+    MatSliderModule,
+    MatSnackBarModule,
+    MatStepperModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatNativeDateModule,
+  ],
 })
-export class AppModule { }
+export class MaterialModule {}
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    StoreModule.forRoot(reducers),
+    MaterialModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, //  Retains last 25 states
+    }),
+  ],
+  declarations: [AppComponent, StickyComponent, ConfirmationDialog],
+  entryComponents: [ConfirmationDialog],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
